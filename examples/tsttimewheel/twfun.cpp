bfbe2027d2bbd6b9efbb32085346963116f69980
yong <51667246@qq.com>
2018-11-13 22:19:12 +0800

    add timewheel tst code

---
+++ examples/tsttimewheel/twfun.cpp
@@ -0,0 +1,75 @@
+
+
+#include "twfun.h"
+
+
+
+
+#include "muduo/base/common.h"
+
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread/thread.hpp>
+
+
+#include "muduo/net/InetAddress.h"
+#include "muduo/net/Channel.h"
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/EventLoopThread.h"
+#include "muduo/net/EventLoopThreadPool.h"
+#include "muduo/net/TcpServer.h"
+#include "muduo/base/Logging.h"
+#include "muduo/net/TcpClient.h"
+
+#include "muduo/base/ThreadPool.h"
+
+
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/PlatformThreadFactory.h>
+
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/protocol/TJSONProtocol.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/server/TNonblockingServer.h>
+
+
+
+
+
+using namespace apache::thrift;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::protocol;
+
+using namespace apache::thrift::concurrency;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::server;
+//using namespace apache::thrift::async;
+
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+
+
+using namespace std;
+using namespace muduo;
+using namespace muduo::net;
+
+
+// timewheel 实现
+
+
+void tst_twfun_entry() {
+    OutputDbgInfo tmpOut( "tst_twfun_entry begin", "tst_twfun_entry end" );
+
+
+}
+
+
