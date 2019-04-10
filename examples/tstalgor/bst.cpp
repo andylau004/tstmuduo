064a0ecb556cf6f39b8b3b1f4d78a0b87c0d0169
yong <51667246@qq.com>
2019-04-10 19:00:35 +0800

    add bst.h bst.cpp

---
+++ examples/tstalgor/bst.cpp
@@ -0,0 +1,252 @@
+
+
+#include "bst.h"
+
+
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+#define N 63
+
+typedef struct node
+{
+    struct node *leftChild;
+    struct node *rightChild;
+    int data;
+    node() :
+        data(0), leftChild(nullptr), rightChild(nullptr) {
+
+    }
+    node(int ival) :
+        data(ival), leftChild(nullptr), rightChild(nullptr) {
+
+    }
+}BiTreeNode, *BiTree;
+
+BiTreeNode* g_newRoot = nullptr;
+
+
+//生成一个结点
+BiTreeNode *createNode(int i)
+{
+    BiTreeNode * q = new BiTreeNode;
+    q->leftChild  = NULL;
+    q->rightChild = NULL;
+    q->data = i;
+    return q;
+}
+void insertNode(BiTreeNode*& tree, BiTreeNode* z) {
+    BiTreeNode* parent = NULL;
+    BiTreeNode* temp = tree;
+    while (temp) {
+        parent = temp;
+        if (z->data > temp->data)
+            temp = temp->rightChild;
+        else
+            temp = temp->leftChild;
+    }
+    if (parent == nullptr) {
+        tree = z;
+    } else if (z->data > parent->data) {
+        parent->rightChild = z;
+    } else {
+        parent->leftChild = z;
+    }
+}
+BiTreeNode* createBiTree1()
+{
+//    BiTreeNode *p[N] = {NULL};
+    std::vector<int> tmpV;
+    tmpV.push_back(87); tmpV.push_back(13); tmpV.push_back(6);
+    tmpV.push_back(99); tmpV.push_back(27); tmpV.push_back(35); tmpV.push_back(12);
+
+    int i = 0;
+    for (i = 0; i < tmpV.size(); ++i) {
+        insertNode( g_newRoot, createNode(tmpV[i]/*i + 10*/) );
+    }
+    return g_newRoot;
+//    for (i = 0; i < N/2; ++i) {
+//        p[i]->leftChild = p[i*2 + 1];
+//        p[i]->leftChild = p[i*2 + 2];
+//    }
+//    return p[0];
+}
+int getBiggestValue(BiTree T) {
+    if (!T) {
+        return 0;
+    }
+    int maxleft = getBiggestValue(T->leftChild);
+    int maxright= getBiggestValue(T->rightChild);
+    std::cout << "maxleft=" << maxleft << ", maxright=" << maxright << std::endl;
+    int max = maxleft > maxright ? maxleft : maxright;
+    std::cout << "max=" << max << std::endl;
+    return max > T->data ? max : T->data;
+}
+void inorderPrint(BiTree root) {
+    if (!root) return;
+    if (root->leftChild)
+        inorderPrint(root->leftChild);
+    std::cout << root->data << " ";
+    if (root->rightChild)
+        inorderPrint(root->rightChild);
+}
+void tst_bst_1() {
+    BiTreeNode* T1 = createBiTree1();
+    std::cout << "lastMax=" << getBiggestValue(T1) << std::endl;
+
+    std::cout << "-------------inOrder print beg" << std::endl;
+    inorderPrint(T1);
+    std::cout << std::endl;
+    std::cout << "-------------inOrder print end" << std::endl;
+}
+
+//给出如下一棵二叉树：
+
+//     1
+//   /   \
+// -5     2
+// / \   /  \
+//0   3 -4  -5
+//返回值为 3 的节点。
+
+BiTreeNode* bst_2_maxnode(BiTreeNode* root) {
+    if (!root) return nullptr;
+
+    BiTreeNode *left, *right;
+    left = right = root;
+
+    if (root->leftChild)
+        left = bst_2_maxnode(root->leftChild);
+    if (root->rightChild)
+        right = bst_2_maxnode(root->rightChild);
+
+    BiTreeNode *childMax = left->data > right->data ? left : right;
+    return root->data > childMax->data ? root : childMax;
+}
+void tst_bst_2() {
+    BiTreeNode* n0 = createNode(1);
+
+    BiTreeNode* n___5;
+    BiTreeNode* n___2;
+    {
+        BiTreeNode* n1 = createNode(-5);
+        BiTreeNode* n2 = createNode(2);
+        n0->leftChild = n1; n0->rightChild = n2;
+        n___5 = n1;
+        n___2 = n2;
+    }
+    {
+        BiTreeNode* n1 = createNode(0);
+        BiTreeNode* n2 = createNode(3);
+        n___5->leftChild = n1; n___5->rightChild = n2;
+    }
+    {
+        BiTreeNode* n1 = createNode(-4);
+        BiTreeNode* n2 = createNode(-5);
+        n___2->leftChild = n1; n___2->rightChild = n2;
+    }
+
+    BiTreeNode* ret = bst_2_maxnode(n0);
+    std::cout << "ret=" << ret->data << std::endl;
+
+    std::cout << "-------------n0 inOrder print beg" << std::endl;
+    inorderPrint(n0);
+    std::cout << std::endl;
+    std::cout << "-------------n0 inOrder print end" << std::endl;
+
+}
+
+
+void tst_bst_tree() {
+//    tst_bst_2();
+//    return;
+//    tst_bst_1();
+//    return;
+
+    BSTree<int> s;
+
+//    int a ;
+//    std::cout << "请输入二叉树结点以构造二叉查找树：" << std::endl;
+    std::vector< int > vec_data = { 10922, 33, 79, 7890,
+                                    7, 1, 4, 2 };
+    for (int i = 0; i < vec_data.size(); ++i) {
+        s.insert(vec_data[i]);
+    }
+//    while(std::cin>>a )
+//        s.insert(a);
+//    std::cin.clear();
+
+    std::cout << "pre order traverse:" << std::endl;
+    s.preOrder();
+    std::cout << std::endl;
+
+    cout<<"in order traverse:"<<endl;
+    s.inOrder();
+    cout<<endl;
+
+    cout<<"post order traverse:"<<endl;
+    s.postOrder();
+    cout<<endl;
+
+    cout<<"print binary search tree ------------- beg"<<endl;
+    s.print();
+    cout<<endl;
+    cout<<"print binary search tree ------------- end"<<endl;
+
+//    std::cin.clear();
+//    std::cout << "xx=" << 89 << std::endl;
+
+//    while ( std::cin >> a ) {// while beg
+//        std::cout << "a=" << a << std::endl;
+
+//        BSTNode< int >* findNode = s.search(a);
+//        if ( !findNode ) {
+
+//        } else {
+//            cout<<findNode->_key<<"查找成功"<<endl;
+//        }
+//    }// while end
+//    std::cin.clear();
+
+    BSTNode<int>* minNode = s.minNode();
+    if (minNode)
+        cout<<"min node key="<<minNode->_key<<endl;
+
+    BSTNode<int>* maxNode = s.maxNode();
+    if (maxNode)
+        cout<<"max node key="<<maxNode->_key<<endl;
+
+    int iDepth = s.Depth();
+    cout<<"iDepth="<<iDepth<<endl;
+    {
+        int calc_tree_layers = s.calc_tree_layers();
+        cout<<"calc_tree_layers="<<calc_tree_layers<<endl;
+    }
+
+    {
+        std::cout << "closet value1=" << s.closestValue(123) << std::endl;
+        std::cout << "closet value2=" << s.closestValue(1) << std::endl;
+    }
+
+    {
+        s.PrintTree_ByLine_1();
+//        s.PrintTree_ByLine();
+    }
+}
+
+
+
+/*
+    *
+    *前序遍历算法
+    *BSTree类内部调用函数
+    *
+*/
+//template < typename T >
+//void BSTree< T >::preOrder() {
+
+//}
---
+++ examples/tstalgor/bst.h
@@ -0,0 +1,534 @@
+
+
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+#include <deque>
+#include <queue>
+
+template < typename T >
+class BSTNode {
+public:
+    T  _key;
+    BSTNode* _lchild;
+    BSTNode* _rchild;
+    BSTNode* _parent;
+
+public:
+    BSTNode(T key, BSTNode* lchild, BSTNode* rchild, BSTNode* parent) :
+        _key( key ),
+        _lchild(lchild),
+        _rchild(rchild),
+        _parent(parent)
+    {}
+};
+
+
+template < typename T >
+class BSTree {
+
+private:
+    BSTNode< T >* _Root;
+
+public:
+    BSTree() : _Root (nullptr) {
+    }
+    ~BSTree() {
+    }
+
+    void insert( T key );
+    BSTNode< T >* search( T key );
+    BSTNode< T >* maxNode();
+    BSTNode< T >* minNode();
+
+    T maxKey();
+    T minKey();
+
+    void preOrder();//先序输出
+    void inOrder(); //中序输出
+    void postOrder();//后序输出
+
+    void print();//打印二叉树
+    void remove(T key);
+
+    BSTNode<T>* predecessor(BSTNode<T>* x);//查找某个结点的前驱
+    BSTNode<T>* sucessor(BSTNode<T>* x); //查找某个结点的后继
+
+    void destory();
+    // 计算二叉树的高度
+    int  Depth();
+
+    // 计算二叉树的层数
+    int  calc_tree_layers();
+
+    // 获取跟输入节点值最接近的节点值
+    T    closestValue(T key);
+
+    // 按行打印二进制查找树
+    void PrintTree_ByLine();
+    // 按行打印二进制查找树
+    void PrintTree_ByLine_1();
+
+    // 内部使用函数，供外部接口调用
+private:
+    T    closestValue(BSTNode<T>*& root, T key);
+
+    int  calc_tree_layers(BSTNode<T>*& root);
+    int  Depth(BSTNode<T>*& tree);
+
+    void insert(BSTNode<T>* &tree,BSTNode<T>* z);
+    BSTNode<T>* search(BSTNode<T>* &tree,T key) const;
+    void preOrder(BSTNode<T>*&tree) const;
+    void inOrder(BSTNode<T>*&tree) const;
+    void postOrder(BSTNode<T>*&tree) const;
+    BSTNode<T>* minimumNode(BSTNode<T> *&tree);
+    BSTNode<T>* maximumNode (BSTNode<T> *&tree);
+    void print(BSTNode<T>*& tree);
+    BSTNode<T>* remove(BSTNode<T>* &tree, BSTNode<T> *z);
+    void destory(BSTNode<T>*& tree);
+
+};
+
+
+
+/*
+*插入操作
+*非递归实现
+*内部使用函数
+*/
+template<typename T>
+void BSTree<T>::insert(BSTNode<T>* &tree, BSTNode<T>* z)
+{
+    BSTNode< T > * parent = NULL;
+    BSTNode< T > * temp = tree;
+
+    while (temp) {
+        parent = temp;
+        if (z->_key > temp->_key) {
+            temp = temp->_rchild;
+        } else {
+            temp = temp->_lchild;
+        }
+    }
+    z->_parent = parent;
+    if (parent == nullptr) {//如果树本来就是空树，则直接把z节点插入根节点
+        tree = z;
+    } else if (z->_key > parent->_key) {//如果z的值大于其双亲，则z为其双亲的右孩
+        parent->_rchild = z;
+    } else {
+        parent->_lchild = z;
+    }
+}
+template <typename T>
+void BSTree<T>::insert(T key)
+{
+    //创建一个新的节点，使用构造函数初始化
+    BSTNode<T>* z= new BSTNode<T>(key, nullptr, nullptr, nullptr);
+    if(!z) //如果创建失败则返回
+        return;
+    //调用内部函数进行插入
+    insert(_Root,z);
+}
+/*
+*查找操作
+*非递归实现
+*内部使用函数
+*/
+template <typename T>
+BSTNode<T>*  BSTree<T>::search(BSTNode<T>* &tree, T key) const
+{
+    BSTNode<T>* temp = tree;
+    while ( temp ) {
+        if ( key == temp->_key )
+            return temp;
+        else if ( key > temp->_key ) {
+            temp = temp->_rchild;
+        } else {
+            temp = temp->_lchild;
+        }
+    }
+    return temp;
+}
+template <typename T>
+BSTNode<T>*  BSTree<T>::search(T key)
+{
+    return search(_Root,key);
+}
+/*
+*
+*前序遍历算法
+*外部使用接口
+*
+*/
+template<typename T>
+void BSTree<T>::preOrder(BSTNode<T>*&tree) const {
+    if ( tree ) {
+        std::cout << tree->_key << " ";
+        preOrder(tree->_lchild);
+        preOrder(tree->_rchild);
+    }
+}
+template<typename T>
+void BSTree<T>::inOrder(BSTNode<T>*&tree) const {
+    if ( tree ) {
+        inOrder(tree->_lchild);
+        std::cout << tree->_key << " ";
+        inOrder(tree->_rchild);
+    }
+}
+template<typename T>
+void BSTree<T>::postOrder(BSTNode<T>*&tree) const {
+    if ( tree ) {
+        postOrder(tree->_lchild);
+        postOrder(tree->_rchild);
+        std::cout << tree->_key << " ";
+    }
+}
+/*
+*遍历算法
+*分别为前序、中序、后序
+*BSTree 类外部接口函数
+*/
+template<typename T>
+void BSTree<T>::preOrder() {
+    preOrder(_Root);
+}
+template<typename T>
+void BSTree<T>::inOrder() {
+    inOrder(_Root);
+}
+template<typename T>
+void BSTree<T>::postOrder() {
+    postOrder(_Root);
+}
+
+/*
+*查找最小的结点
+*内部调用函数
+*/
+template <typename T>
+BSTNode<T>* BSTree<T>::minimumNode(BSTNode<T>*&tree)
+{
+    if (tree == nullptr) return nullptr;
+
+    BSTNode< T >* temp = tree;
+    while (temp->_lchild) {
+        temp = temp->_lchild;
+    }
+    return temp;
+}
+template<typename T>
+BSTNode<T>* BSTree<T>::minNode()
+{
+    return minimumNode(_Root);
+}
+/*
+*
+*查找键值最大的节点
+*内部调用函数
+*非递归实现
+*/
+template<typename T>
+BSTNode<T>* BSTree<T>::maximumNode(BSTNode<T>* &tree) {
+    if (tree == nullptr) return nullptr;
+
+    BSTNode<T>* temp = tree;
+    while(temp->_rchild) {
+        temp = temp->_rchild;
+    }
+    return temp;
+}
+template<typename T>
+BSTNode<T>* BSTree<T>::maxNode() {
+    return maximumNode(_Root);
+}
+
+/*
+*
+*查找最小的键值
+*外部接口函数
+*调用内部函数minimumNode实现
+*/
+template<typename T>
+T BSTree<T>::minKey()
+{
+    BSTNode<T> *temp = minimumNode(_Root);
+    return temp->_key;
+}
+
+/*
+*
+*查找最大的键值
+*外部接口函数
+*调用内部函数maximumKey
+*/
+template<typename T>
+T BSTree<T>::maxKey()
+{
+    BSTNode<T> *temp = maximumNode(_Root);
+    return temp->_key;
+}
+
+/*
+*
+*打印函数
+*打印出平衡二叉树
+*BStree内部函数
+*/
+template<typename T>
+void BSTree<T>::print(BSTNode<T>*& tree)
+{
+    if (!tree) return;
+
+    if (tree->_lchild) {
+        std::cout<<"节点"<<tree->_key<<"有左孩子为"<<tree->_lchild->_key<<std::endl;
+    } else {
+        std::cout<<"节点"<<tree->_key<<"无左孩子"<<std::endl;
+    }
+    if (tree->_rchild) {
+        std::cout<<"节点"<<tree->_key<<"有右孩子为"<<tree->_rchild->_key<<std::endl;
+    } else {
+        std::cout<<"节点"<<tree->_key<<"无右孩子"<<std::endl;
+    }
+    print(tree->_lchild);
+    print(tree->_rchild);
+}
+
+template<typename T>
+void BSTree<T>::print() {
+    print(_Root);
+}
+
+template<typename T>
+int BSTree<T>::Depth(BSTNode<T>*& tree)
+{
+    int depth;
+    int depthLeft, depthRigth;
+
+    if (!tree) return 0;
+    depthLeft  = Depth(tree->_lchild);
+    depthRigth = Depth(tree->_rchild);
+    depth = 1 + (depthLeft > depthRigth ? depthLeft : depthRigth);
+    return depth;
+}
+template<typename T>
+int BSTree<T>::Depth() {
+    return Depth(_Root);
+}
+
+template<typename T>
+int BSTree<T>::calc_tree_layers() {
+    return calc_tree_layers(_Root);
+}
+template<typename T>
+int BSTree<T>::calc_tree_layers(BSTNode<T>*& root)
+{
+    int left_way_layers, right_way_layers;
+    if (root==NULL)    return 0;    //递归出口，空结点的层数是0
+    else {
+        left_way_layers  = 1 + calc_tree_layers(root->_lchild);    //从左路返上来的层数
+        right_way_layers = 1 + calc_tree_layers(root->_rchild);     //从右路返上来的层数
+        if(left_way_layers>right_way_layers) return left_way_layers;
+        else return right_way_layers;
+    }
+}
+
+// 获取 跟输入数据 最接近的节点值
+template<typename T>
+T BSTree<T>::closestValue(BSTNode<T>*& root, T target)
+{
+    T a = root->_key;
+    BSTNode<T>* t = target < a ? root->_lchild : root->_rchild;
+    if (!t) return a;
+    T b = closestValue(t, target);
+    return abs(target - a) < abs(target - b) ? a : b;
+}
+template<typename T>
+T BSTree<T>::closestValue(T key) {
+    return closestValue(_Root,key);
+}
+
+// 按行打印二进制查找树
+template<typename T>
+void BSTree<T>::PrintTree_ByLine_1() {
+    if (!_Root) return;
+
+    std::queue< BSTNode<T>* > qNodes;
+    qNodes.push(_Root);
+
+    int curline = 1;
+
+    while (!qNodes.empty()) {
+        int nexline = 0;
+
+        for (; curline > 0; -- curline) {
+            BSTNode<T>* pNode = qNodes.front();
+            qNodes.pop();
+            std::cout << pNode->_key << "\t";
+
+            if (pNode->_lchild) {
+                qNodes.push(pNode->_lchild);
+                nexline ++;
+            }
+            if (pNode->_rchild) {
+                qNodes.push(pNode->_rchild);
+                nexline ++;
+            }
+        }
+
+        curline = nexline;
+        std::cout << "\n";
+    }
+
+}
+
+template<typename T>
+void BSTree<T>::PrintTree_ByLine() {
+    if (!_Root) return;
+
+    std::queue< BSTNode<T>* > queueNodes;
+
+    BSTNode<T>* last  = _Root;
+    BSTNode<T>* nlast = _Root;
+
+    queueNodes.push( _Root );
+
+    while (!queueNodes.empty()) {
+        BSTNode<T>* pNode = queueNodes.front();
+        queueNodes.pop();
+        std::cout << pNode->_key << "\t";
+
+        if (pNode->_lchild) {
+            queueNodes.push(pNode->_lchild);
+            nlast = pNode->_lchild;
+        }
+        if (pNode->_rchild) {
+            queueNodes.push(pNode->_rchild);
+            nlast = pNode->_rchild;
+        }
+
+        if (pNode == last) {
+            last = nlast;
+            std::cout << std::endl;
+        }
+    }
+}
+
+/*
+*查找某个节点x的前驱
+*
+*外部函数调用
+*
+*/
+template <typename T>
+BSTNode<T>* BSTree<T>::predecessor(BSTNode<T>* x)
+{
+    //如果x是最小的结点，则它没有前驱
+    if (x->_key == minimumNode(_Root)->_key)
+        return NULL;
+
+    //否则
+    //先获取二叉树中键值与x的键值相同的结点y
+    BSTNode<T>* y = search(_Root, x->_key);
+    if (!y)
+        return nullptr;
+
+    //如果y有左孩子，则x的前驱为“以x的左孩为根的子树的最大结点”
+    if (y->_lchild)
+        return maximumNode(y->_lchild);
+
+    //如果y没有左孩子，则x有两种可能：
+    //1.y是一个右孩子，此时x的前驱为其双亲节点
+    BSTNode<T>* parent = y->_parent;
+    if (parent->_rchild == y)
+        return parent;
+
+    //2.y是一个左孩子，则其前驱为其双亲结点中“第一个拥有右孩子结点”的结点
+    while (parent && parent->_rchild == nullptr) {
+        parent = parent->_parent;
+    }
+    return parent;
+}
+/*
+*查找某个节点x的后继
+*
+*外部调用接口
+*
+*/
+template <typename T>
+BSTNode<T>* BSTree<T>::sucessor(BSTNode<T>* x)
+{
+    //如果x是键值最大的，则x没有后继结点
+    if (x->_key == maximumNode(_Root))
+        return nullptr;
+
+    //获取x在二叉树中的结点y
+    BSTNode<T>* y = nullptr;
+    y = search(_Root, x->_key);
+    if (!y)
+        return nullptr;//若二叉树没有此结点
+
+    //如果y有右孩子，则y的后继为其右孩子的最小结点
+    if (y->_rchild)
+        return minimumNode(y->_rchild);
+
+    //如果y没有右孩子，则可分为两种情况：
+    //1.y 是左孩子。此时y的后继为y的父结点
+    BSTNode<T>* parent = y->_parent;
+    if (parent->_lchild == y)
+        return parent;
+
+    //2.y是右孩子。此时y的后继结点为“第一个拥有左孩且不是y的直接双亲”的结点
+    while (parent) {
+        if (parent->_lchild && parent != y->_parent)
+            return parent;
+        parent = parent->_parent;
+    }
+    return nullptr;
+}
+/*
+*
+*删除结点
+*BSTree类内部调用函数
+*
+*/
+template <class T>
+BSTNode<T>* BSTree<T>::remove(BSTNode<T>* &tree, BSTNode<T> *z)
+{
+    BSTNode<T> *x=NULL;
+    BSTNode<T> *y=NULL;
+    return nullptr;
+}
+template<typename T>
+void BSTree<T>::remove(T key)
+{
+    BSTNode<T> *z, *node;
+    if ((z = search(_Root, key)) != NULL)
+        if ((node = remove(_Root, z)) != NULL)
+            delete node;
+}
+/*
+*销毁查找二叉树
+*内部调用函数
+*/
+template<typename T>
+void BSTree<T>::destory(BSTNode<T>*& tree)
+{
+    if (tree->_lchild)
+        destory(tree->_lchild);
+    if (tree->_rchild)
+        destory(tree->_rchild);
+    if (tree->_lchild && tree->_rchild) {
+        delete tree;
+        tree = nullptr;
+    }
+}
+template<typename T>
+void BSTree<T>::destory() {
+    destory(_Root);
+}
+
+
+extern void tst_bst_tree();
