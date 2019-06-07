52fe6ea6af3bee47e35a39a358e9397d14927cfa
fanyong <51667246@qq.com>
2019-06-07 17:41:13 +0800

    add new modify

--- muduo/base/Exception.h
+++ muduo/base/Exception.h
@@ -7,7 +7,12 @@
 #define MUDUO_BASE_EXCEPTION_H
 
 #include <muduo/base/Types.h>
+
+
+#include <string>
+#include <sstream>
 #include <exception>
+
 
 namespace muduo
 {
@@ -28,6 +33,37 @@ class Exception : public std::exception
   string stack_;
 };
 
+
+
+
 }
+
+
+class ErasureException : public std::exception
+{
+public:
+    ErasureException(int errcode, const std::string& verbose = "") throw()
+    :std::exception(), mErrorCode(errcode), mVerbose(verbose)
+    {
+        std::ostringstream oss;
+        oss << "Error Code: " << mErrorCode << " " << mVerbose;
+        mWhat = oss.str();
+    }
+
+    virtual ~ErasureException() throw() {}
+
+public: // From std::exception
+    virtual const char* what() const throw() { return mWhat.c_str(); }
+
+public:
+    int get_error_code() const throw() { return mErrorCode; }
+
+    const std::string& get_verbose() const throw() { return mVerbose; }
+
+protected:
+   int mErrorCode;
+   std::string mWhat;
+   std::string mVerbose;
+};
 
 #endif  // MUDUO_BASE_EXCEPTION_H
