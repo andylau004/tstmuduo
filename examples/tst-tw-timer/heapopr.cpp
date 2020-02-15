4c89fb049278faf12148f96df8c1caf0879dd8e1
fanyong <51667246@qq.com>
2020-02-16 00:21:19 +0800

    add new work cpp h

--- examples/tst-tw-timer/heapopr.cpp
+++ examples/tst-tw-timer/heapopr.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+
 
 #include "muduo/base/common.h"
 
@@ -44,6 +46,7 @@ void timer_heap::add_timer(heap_timer_node *timer) {
     /* 调整堆 */
     push_heap(array.begin(), array.begin() + cur_size, cmp_heap_timer_node);
 }
+
 void timer_heap::del_timer(heap_timer_node *timer) {
     if (!timer) return;
 
@@ -56,17 +59,17 @@ void timer_heap::del_timer(heap_timer_node *timer) {
     /* 判断删除数是否达到当前容量的一半，达到了则删除并重新建堆 */
     if (del_num >= cur_size/2) {
 
-        int validIndex = 0;
+        int vaildIndex = 0;
         for ( int i = 0; i < cur_size; i ++ ) {
             if (array[i]->validFlag) {
-                array[validIndex] = array[i];
-                validIndex ++;
+                array[vaildIndex] = array[i];
+                vaildIndex ++;
             } else {
                 delete array[i];
             }
             array[i] = nullptr;
         }
-        cur_size = validIndex;
+        cur_size = vaildIndex;
 
         make_heap(array.begin(), array.begin() + cur_size, cmp_heap_timer_node);
     }
@@ -92,8 +95,20 @@ void timer_heap::del_top() {
 }
 
 void timer_heap::tick() {
-//    heap_timer_node* topTimer = get_top();
-
+    heap_timer_node* topTimer = get_top();
+    time_t cur = time(nullptr);
+    while (!is_empty()) {
+        if (topTimer == nullptr)
+            break;
+        /* 如果对顶元素还没有到期，则退出循环 */
+        if (topTimer->expire > cur)
+            break;
+        /* 否则执行堆顶定时器中的任务 */
+        if (topTimer->validFlag && topTimer->cb_func)
+            topTimer->cb_func(topTimer->user_data);
+        del_top();
+        topTimer == get_top();
+    }
 }
 
 void timer_heap::resize() {
@@ -131,18 +146,92 @@ void printVec(vector<int> nums) {
     cout << endl;
 }
 
+template <class T>
+void printPriority(T& container) {
+    while (!container.empty()) {
+        cout << container.top() << ' ';
+        container.pop();
+    }
+    cout << endl;
+}
+
 void tst_heapopr_entry() {
 
     {
-        int nums_temp[] = { 8, 3, 4, 8, 9, 2, 3, 4 };
-        cout << "len=" << array_size(nums_temp) << std::endl;
-        return;
+        int myints[] = { 10, 20, 30, 6, 16 };
 
-        vector<int> nums(nums_temp, nums_temp + 8);
+        std::vector<int> vec(myints, myints+5);
+    //    std::vector::iterator it;
+
+        make_heap(vec.begin(), vec.end());
+        std::cout << "initial max heap=" << vec.front() << std::endl;
+
+        pop_heap(vec.begin(), vec.end());
+        vec.pop_back();
+        cout << "max heap after pop : " << vec.front() << endl;
+
+        vec.push_back(99);
+        push_heap(vec.begin(), vec.end());
+        cout << "max heap after push: " << vec.front() << endl;
+        return;
+    }
+
+    {
+        std::priority_queue < int > a;// 默认是大顶堆
+        std::priority_queue < int, std::vector<int>, greater<int> > c;// 小顶堆
+
+        for (int i = 0; i < 5; i++)  {
+            a.push(i);
+            c.push(i);
+        }
+        printPriority(a);
+        printPriority(c);
+
+        return ;
+    }
+
+    {
+        int nums_temp[] = { 8, 3, 4, 8, 9, 2, 3, 4 };
+        int lenNums = array_size(nums_temp);
+        cout << "len=" << lenNums << std::endl;
+//        return;
+
+        vector<int> nums(nums_temp, nums_temp + lenNums);
         cout << "sorce nums: "; printVec(nums);
         cout << "(默认)make_heap: ";
         make_heap(nums.begin(), nums.end()); //默认是大顶堆，只是将最大值的元素和第一个元素互换,其他元素的顺序不变
         printVec(nums);
+
+        cout << "(less)make_heap: ";
+        make_heap(nums.begin(), nums.end(), less<int>()); //创建大顶堆，只是将最大值的元素和第一个元素互换,其他元素的顺序不变
+        printVec(nums);
+
+        cout << "(greater)make_heap: ";
+        make_heap(nums.begin(), nums.end(), greater<int>()); //创建小顶堆，只是将最小值的元素和第一个元素互换,其他元素的顺序不变
+        printVec(nums);
+
+        cout << "此时，nums为小顶堆, push_back(3)" << endl;
+        nums.push_back(3); //在vector的末尾添加元素
+        cout << "忽略第三个参数，即为默认)push_heap: ";
+
+        //下面的一行代码，相当于是往vector中添加元素后更新heap,执行到此处会出现错误，只有make_heap（）和push_heap（）同为大顶堆或小顶堆,才能更新。
+        //但是此处push_heap使用默认参数，是更新大顶堆，但是上面最后一个make_heap创建的是小顶堆，冲突。
+//        push_heap(nums.begin(), nums.end());
+        printVec(nums);
+        cout << "第三个参数为greater: ";
+        push_heap(nums.begin(), nums.end(), greater<int>()); //指定参数后不发生冲突
+        printVec(nums);
+        cout << "(做替换)默认参数pop_heap: ";
+        //pop_heap()并没有删除元素，而是将堆顶元素和数组最后一个元素进行了替换。和上面的push_heap一样第三个参数不指定的话，会发生冲突
+        //pop_heap(nums.begin(), nums.end());
+        printVec(nums);
+        cout << "(做替换)第三个参数为greater,pop_heap: ";
+        pop_heap(nums.begin(), nums.end(), greater<int>()); //指定参数后不发生冲突
+        printVec(nums);
+        cout << "pop_back(): ";
+        nums.pop_back(); //删除vector末尾的元素
+        printVec(nums);
+
         return;
     }
     {
@@ -181,21 +270,6 @@ void tst_heapopr_entry() {
         return;
     }
 
-    int myints[] = { 10, 20, 30, 6, 16 };
-
-    std::vector<int> vec(myints, myints+5);
-//    std::vector::iterator it;
-
-    make_heap(vec.begin(), vec.end());
-    std::cout << "initial max heap=" << vec.front() << std::endl;
-
-    pop_heap(vec.begin(), vec.end());
-    vec.pop_back();
-    cout << "max heap after pop : " << vec.front() << endl;
-
-    vec.push_back(99);
-    push_heap(vec.begin(), vec.end());
-    cout << "max heap after push: " << vec.front() << endl;
 
 
 
